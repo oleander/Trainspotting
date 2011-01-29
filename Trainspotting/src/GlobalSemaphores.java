b128d23f6186585d9e2fb30fdf6e6a3acd70b9c8
Arash Rouhani-Kalleh <rarash@nc-2504-21.studat.chalmers.se>
2011-01-29 15:03:05 +0100

    kommenterade GlobalSemaphores

--- Trainspotting/src/GlobalSemaphores.java
+++ Trainspotting/src/GlobalSemaphores.java
@@ -2,11 +2,21 @@
 import java.util.HashMap;
 import java.util.concurrent.Semaphore;
 
+/**
+ * A global store with the semaphores, the semaphore is not going to get created
+ * until a train actually needs it.
+ */
 public class GlobalSemaphores {
 
     private static HashMap<Point, Semaphore> semaphores = new HashMap<Point, Semaphore>();
 
-    // must be symchronized so not 2 sempahores get created at same time.
+    // must be synchronized so not 2 sempahores get created at same time.
+    /**
+     * Get a segment for a 4-crossing
+     *
+     * @param p point of railwaycrossing
+     * @return semaphore corresponding to 4-crossing
+     */
     public static synchronized Semaphore findOrCreate(Point p) {
         if (semaphores.containsKey(p)) {
             Semaphore s = semaphores.get(p);
@@ -21,14 +31,23 @@ public class GlobalSemaphores {
         }
     }
 
-    static Semaphore findOrCreate2(Point p1, Point p2) {
+    /**
+     * Get segemntial semaphore.
+     *
+     * p1 and p2 SHOULD BE PREHACKED BY CALLER (x := dirCameWith * 1000 + x)
+     *
+     * @param p1 one endpoint of railway (must be prehacked)
+     * @param p2 other endpoint of railway (must be prehacked)
+     * @return semaphore corresponding to given segment
+     */
+    public static Semaphore findOrCreate2(Point p1, Point p2) {
         return findOrCreate(sortHackMerge(p1, p2));
     }
 
-    static Point sortHackMerge(Point p1, Point p2){
-        if(1000*p1.x+p1.y > 1000*p2.x+p2.y){
+    private static Point sortHackMerge(Point p1, Point p2) {
+        if (1000 * p1.x + p1.y > 1000 * p2.x + p2.y) {
             return sortHackMerge(p2, p1);
         }
-        return new Point(p1.x*1000+p1.y, p2.x*1000+p2.y);
+        return new Point(p1.x * 1000 + p1.y, p2.x * 1000 + p2.y);
     }
 }
