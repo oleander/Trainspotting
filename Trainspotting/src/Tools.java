78e36ab2e35393bfe3efa7cf24bd40d5a85dc055
Arash Rouhani-Kalleh <rarash@nc-2504-21.studat.chalmers.se>
2011-01-29 15:04:12 +0100

    kommenterade Tools

--- Trainspotting/src/Tools.java
+++ Trainspotting/src/Tools.java
@@ -1,14 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ * Methods not fitting anywhere else
  *
- * @author arash
  */
 public class Tools {
 
+    /**
+     * like monoidical plus
+     */
     public static Runnable plusActions(final Runnable r1, final Runnable r2){
         return new Runnable() {
             public void run() {
